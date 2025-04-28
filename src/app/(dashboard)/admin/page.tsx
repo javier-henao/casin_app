c1de06446048d47e6908eb7380c28b0174f1bb45
javierhenaohe <javier.henao@outlook.com>
2025-04-28 02:23:18 -0500

    Se visualiza el dato real desde la BD en los Events

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,23 +1,25 @@
 import Announcements from "@/components/Announcements";
-// import AttendanceChart from "@/components/AttendanceChart";
 import AttendanceChartContainer from "@/components/AttendanceChartContainer";
-// import CountChart from "@/components/CountChart";
 import CountChartContainer from "@/components/CountChartContainer";
-import EventCalendar from "@/components/EventCalendar";
+import EventCalendarContainer from "@/components/EventCalendarContainer";
 import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
-const AdminPage = () => {
+
+const AdminPage = ({
+  searchParams,
+}: {
+  searchParams: { [keys: string]: string | undefined };
+}) => {
   return (
-    <div className="flex-1 p-4 flex gap-4 flex-col md:flex-row">
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
-        <div className="flex gap-4 justify-between">
+        <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="admin" />
-          <UserCard type="student" />
           <UserCard type="teacher" />
+          <UserCard type="student" />
           <UserCard type="parent" />
-          {/* <UserCard type="proffesional" /> */}
         </div>
         {/* MIDDLE CHARTS */}
         <div className="flex gap-4 flex-col lg:flex-row">
@@ -30,14 +32,14 @@ const AdminPage = () => {
             <AttendanceChartContainer />
           </div>
         </div>
-        {/* BOTTOM CHARTS */}
+        {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
           <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
       <div className="w-full lg:w-1/3 flex flex-col gap-8">
-        <EventCalendar />
+        <EventCalendarContainer searchParams={searchParams} />
         <Announcements />
       </div>
     </div>
